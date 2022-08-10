0e67874b5d535d18a744eeba05649e1a74719bc9
unknown <josebright29@gmail.com>
2022-08-10 16:18:36 +0000

    first commit to serverless application

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'v3mafe7kme';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-x4-wh6ff.us.auth0.com',
+  clientId: 'Bb8I16pJfxsuvSQf8SEqqR8fMyOpULrD',
+  callbackUrl: 'http://localhost:3000/callback'
+};
